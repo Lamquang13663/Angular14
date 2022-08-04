f978a5aa1c681943497bccf61fa3e7a65df843da
Quanglam <Quanglam@gmail.com>
2022-08-04 21:26:58 +0700

    databinding

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,7 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-
+  public name = 'Quang Lam';
+  public class = 'se1409';
   constructor() { }
 
   ngOnInit(): void {
